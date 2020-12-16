950edf9f412260bbeb0c920b9170ecb90e403b4f
Jesse Solis <jesse.solis0316@gmail.com>
2020-12-16 12:54:26 -0600

    set up appollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
